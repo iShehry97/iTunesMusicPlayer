d232c2ee44b982d17a3ee01143cb29ce0ee1cb37
Syed Shaheer Sarwar <engr.shaheer97@hotmail.com>
2019-11-25 23:34:52 +0500

    first commit

---
+++ ShuffleMusicApp/GenreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GenreViewController.swift
+//  ShuffleMusicApp
+//
+//  Created by Syed Shaheer Sarwar on 11/25/19.
+//  Copyright © 2019 Syed Shaheer Sarwar. All rights reserved.
+//
+
+import UIKit
+
+class GenreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
