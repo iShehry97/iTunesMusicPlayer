dc8f9d3efa26fff5f46953e38da0b7068ea75fb7
Syed Shaheer Sarwar <engr.shaheer97@hotmail.com>
2019-11-25 18:53:53 +0500

    Initial Commit

---
+++ ShuffleMusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShuffleMusicApp
+//
+//  Created by Syed Shaheer Sarwar on 11/25/19.
+//  Copyright © 2019 Syed Shaheer Sarwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
